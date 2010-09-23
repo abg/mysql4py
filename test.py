132733270f1c9b55ab0caa3984a0126bae1d2b22
Andrew Garner <muzazzi@gmail.com>
2010-09-23 03:56:54 +0000

    update test to use modified protocol query api

--- test.py
+++ test.py
@@ -4,23 +4,23 @@ from mysql4py.protocol import Protocol
 endpoint = connect_unix('/var/lib/mysql/mysql.sock')
 proto = Protocol(endpoint)
 #proto.enable_ssl()
-proto.enable_compression()
+#proto.enable_compression()
 proto.authenticate(user='root')
-proto.query('SHOW GRANTS')
-proto.fields()
-for row in proto.rows():
-    print row
-print (proto.query("SHOW SESSION STATUS LIKE '%SSL%'"))
-print (proto.fields())
-for row in proto.rows():
-    print (row)
+result = proto.query('SHOW GRANTS')
 
-proto.query('SHOW SESSION variables LIKE "%char%"')
-print proto.fields()
-for row in proto.rows():
+if result:
+    for row in result:
+        print row
+
+result = proto.query("SHOW SESSION STATUS LIKE '%SSL%'")
+if result:
+    for row in result:
+        print (row)
+
+result = proto.query('SHOW SESSION variables LIKE "%char%"')
+for row in result:
     print [row for row in row]
 
-proto.query('SELECT 1, NOW(), "foo bar baz"')
-print proto.fields()
-for row in proto.rows():
+result = proto.query('SELECT 1, NOW(), "foo bar baz"')
+for row in result:
     pass
