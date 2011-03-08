30afe49737a17fe9da6c341392ba769967342255
Andrew Garner <muzazzi@gmail.com>
2011-03-08 14:13:23 -0500

    make setup.cfg work better

--- test.py
+++ test.py
@@ -1,26 +1,13 @@
-from mysql4py.channel import connect_unix
-from mysql4py.protocol import Protocol
+import mysql4py
 
-endpoint = connect_unix('/var/lib/mysql/mysql.sock')
-proto = Protocol(endpoint)
-#proto.enable_ssl()
-#proto.enable_compression()
-proto.authenticate(user='root')
-result = proto.query('SHOW GRANTS')
+c = mysql4py.connect(read_default_group='client')
+z = c.cursor()
+z.execute('SHOW GLOBAL VARIABLES LIKE %s', ('socket',))
+import pprint
+pprint.pprint(list(z))
 
-if result:
-    for row in result:
-        print row
+z = c.cursor()
 
-result = proto.query("SHOW SESSION STATUS LIKE '%SSL%'")
-if result:
-    for row in result:
-        print (row)
+z.execute('SELECT USER()', ())
 
-result = proto.query('SHOW SESSION variables LIKE "%char%"')
-for row in result:
-    print [row for row in row]
-
-result = proto.query('SELECT 1, NOW(), "foo bar baz"')
-for row in result:
-    pass
+pprint.pprint(list(z))
