8010bccc2819184982dd3931a89e3f90ca7eb1bd
Andrew Garner <muzazzi@gmail.com>
2011-04-17 04:46:47 -0400

    expose standard dbapi exceptions in top-level module

--- mysql4py/__init__.py
+++ mysql4py/__init__.py
@@ -1,1 +1,6 @@
 from dbapi import Connection as connect, paramstyle, apilevel, threadsafety
+from errors import Warning, Error, \
+                   InterfaceError, DatabaseError, \
+                   InternalError, OperationalError, IntegrityError, \
+                   InternalError, ProgrammingError, NotSupportedError
+MySQLError = DatabaseError
